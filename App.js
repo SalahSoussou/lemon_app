ce07080e9e230f8cf8918d54df4c008c95428a38
SalahSoussou <soussousalahiddine@gmail.com>
2023-03-08 13:51:29 +0100

    ok

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default App = () => {
   const getMenu = async () => {
     try {
       const response = await fetch(
-        "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonSimpleMenu.json"
+        "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu-items-by-category.json"
       );
       const json = await response.json();
       setData(json.menu);
